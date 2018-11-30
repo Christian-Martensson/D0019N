230961026a10a21e313bfe75068d91c0109707fa
Christian Mårtensson <Christian.marten@hotmail.com>
2018-11-30 12:08:27 +0100

    Commit 3 - relativt färdig personnummer kontroll

--- src/ExUpg2/GymWakeUp_Main.java
+++ src/ExUpg2/GymWakeUp_Main.java
@@ -2,20 +2,20 @@ package ExUpg2;
 import java.util.Scanner;
 
 /* TO DO
-* Allow personal ID with and without dash
-* Allow invalid input to be repeated right away (do while)
-*
-*
-*
-* */
+ * Allow personal ID with and without dash
+ * Allow invalid input to be repeated right away (do while)
+ * Kontrollera tillåtna siffror för månad och dag (ungefärligt)
+ *
+ *
+ * */
 
 
 public class GymWakeUp_Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        GymWakeUp_Main instance = new GymWakeUp_Main();
+        GymWakeUp_Main_Test instance = new GymWakeUp_Main_Test();
 
-        instance.getMenu();
+        System.out.println(instance.regID());
 
         //instance.RegID();
 
@@ -26,67 +26,77 @@ public class GymWakeUp_Main {
         //instance.checkID(persNr);
 
 
-        /*
-        String[][] room = new String[3][3];
-        for (int i=0, i<3, i++) {
-            for (int j=0, j<3, j++) {
-                room[i][j] = " ";
-            }
 
-        }
-        */
     }
 
     public static String regID() {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter your personal ID number:");
-        String persNrTemp = input.next();
-        char[] xytemp = persNrTemp.toCharArray();
-
         int sumLoop = 0;
+        String persNr = "";
+        String persNrTemp;
+        Scanner input = new Scanner(System.in);
+        boolean check = true;
 
-        if (xytemp.length !=11 || !(xytemp[6] == '-' || xytemp[6] == '+' )){
-            System.out.println("You need to enter the personal ID number on the form: yymmdd-xxxx");
+        do {
+            // Input for personal ID number
+            System.out.println("Enter your personal ID number:");
+            // Assigns input to variable persNrTemp
+            persNrTemp = input.next();
+            // Converts the string to a charArray
+            char[] xytemp = persNrTemp.toCharArray();
+
+            // IF written on dash-form, convert to non-dash form.
+            if (xytemp.length == 11 && (xytemp[6] == '-' || xytemp[6] == '+')) {
+                persNr = persNrTemp.replace("-", "");
+                char[] xy = persNr.toCharArray();
+                check = false;
+            } else if (xytemp.length == 10) {
+                persNr = persNrTemp;
+                char[] xy = persNr.toCharArray();
+                check = false;
+            } else {
+                System.out.println("You need to enter the personal ID number on the form: yymmdd-xxxx");
+            }
+        } while (check);
+
+        char[] xy = persNr.toCharArray();
+
+
+
+        // Multiply every other number by 2 (starting with the very first number) and add each to a sum variable.
+        // If the product consists of two numbers, add them together.
+        for (int x = 0; x < 10; x = x + 2) {
+            int number = xy[x] - '0';
+            if (number * 2 >= 10) {
+                int product = number * 2;
+                String productAsString = Integer.toString(product);
+                char[] mini = productAsString.toCharArray();
+                int firstNumber = mini[0] - '0';
+                int secondNumber = mini[1] - '0';
+                int summan = firstNumber + secondNumber;
+                sumLoop = sumLoop + summan;
+            }
+            else {
+                sumLoop = sumLoop + number * 2;
+            }
         }
 
+        // Add every other number to a sum variable (starting with the second number)
+        for (int x = 1; x < 9; x = x + 2) {
+            int number = xy[x] - '0';
+            sumLoop = sumLoop + number;
+        }
 
+        int sistaSiffran = xy[9] - '0';
+        int allt = sumLoop + sistaSiffran;
+        System.out.println("Summan: " + sumLoop);
+        System.out.println("Summan, inklusive kontrollsiffran: " + allt);
+
+        if (allt % 10 == 0)
+            return persNr;
         else {
-            String persNr = persNrTemp.replace("-", "");
-            char[] xy = persNr.toCharArray();
-            // Kontrollera tillåtna siffror för måndag och dag (ungefärligt)
-            // If uneven,
-            for (int x = 0; x < 10; x = x + 2) {
-                int number = xy[x] - '0';
-                if (number * 2 >= 10) {
-                    int product = number * 2;
-                    String productAsString = Integer.toString(product);
-                    char[] mini = productAsString.toCharArray();
-                    int firstNumber = mini[0] - '0';
-                    int secondNumber = mini[1] - '0';
-                    int summan = firstNumber + secondNumber;
-                    sumLoop = sumLoop + summan;
-                }
-                else {
-                    sumLoop = sumLoop + number * 2;
-                }
-            }
-
-
-            for (int x = 1; x < 9; x = x + 2) {
-
-                int number = xy[x] - '0';
-                sumLoop = sumLoop + number;
-            }
-
-            int sistaSiffran = xy[9] - '0';
-            int allt = sumLoop + sistaSiffran;
-            System.out.println("Summan: " + sumLoop);
-            System.out.println("Summan, inklusive kontrollsiffran: " + allt);
-
-
+            System.out.println("Error");
+            return "Error";
         }
-        return persNrTemp;
     }
 
     public void checkID(String persNr) {
@@ -123,7 +133,7 @@ public class GymWakeUp_Main {
                 }
                 else {
                     monthlyPrice = 250;
-                    }
+                }
 
             }
         }
@@ -131,8 +141,8 @@ public class GymWakeUp_Main {
         totalPrice = monthlyPrice * m;
 
         System.out.printf("\nThe membership fee is %d. " +
-                "\nYour monthly fee is %d crowns." +
-                "\nOver a period of %d months you will pay %d crowns in total.",
+                        "\nYour monthly fee is %d crowns." +
+                        "\nOver a period of %d months you will pay %d crowns in total.",
                 membership, monthlyPrice, m, totalPrice);
     }
 
@@ -164,6 +174,17 @@ public class GymWakeUp_Main {
     }
     private void becomeMember() {
 
+    }
+    private void bookaclass (String placement){
+          /*
+        String[][] room = new String[3][3];
+        for (int i=0, i<3, i++) {
+            for (int j=0, j<3, j++) {
+                room[i][j] = " ";
+            }
+
+        }
+        */
     }
 }
 
